90a5c0a61742d2352435f5ea44ab1368e1cd18e3
Alexandru Bracau <alex@brc.xyz>
2026-03-14 12:12:51 +0200

    Cask audit URL verification

--- Casks/dsfm.rb
+++ Casks/dsfm.rb
@@ -2,7 +2,8 @@ cask "dsfm" do
   version "0.1.1"
   sha256 "b36a9abe546b33b13f7fcde797e330758fef15973a6d61a5d4dbd98cea0555e8"
 
-  url "https://github.com/alex-brc/dsfm/releases/download/v#{version}/DSFM-#{version}.dmg"
+  url "https://github.com/alex-brc/dsfm/releases/download/v#{version}/DSFM-#{version}.dmg",
+      verified: "github.com/alex-brc/dsfm/"
   name "DualSense for Mac"
   desc "Unlocks the full input surface of PS5 DualSense controllers over Bluetooth"
   homepage "https://brc.xyz/"
